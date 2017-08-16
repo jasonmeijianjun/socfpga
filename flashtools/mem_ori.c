c0cd12a8e963d9a4ee48bf5f1475d0b62340df4f
Jason Mei <jianjun.mei@broadcom.com>
2017-08-16 19:31:54 +0800

    Added the orinal memory read

---
+++ flashtools/mem_ori.c
@@ -0,0 +1,60 @@
+#include<stdio.h>  
+#include<unistd.h>  
+#include<sys/mman.h>  
+#include<sys/types.h>  
+#include<sys/stat.h>  
+#include<fcntl.h> 
+#include <stdlib.h>
+ 
+//#define PHY_BASE 0x171d4a000 
+#define SIZE 0x4000
+int main(int argc,char *argv[])  
+{  
+	  unsigned char * map_base;  
+	  FILE *f;  
+	  int n, fd;  
+	  int i;
+	  unsigned long long PHY_BASE;
+									  
+	unsigned long addr;  
+	unsigned char content;  
+
+	char *stop;
+           if (argc < 2) {
+               fprintf(stderr, "Usage: %s str [base]\n", argv[0]);
+               exit(-1);
+           }
+
+
+	  PHY_BASE = strtoll(argv[1], &stop, 16);
+	printf("ori---0x%llx---\n", PHY_BASE);
+//	  PHY_BASE += 0x100000000;
+	  
+//	printf("after 0x%llx\n", PHY_BASE);
+
+
+		  fd = open("/dev/mem", O_RDWR);  
+			if (fd == -1)  {  
+					return (-1);  
+			}  
+						  
+			 map_base = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, PHY_BASE);  
+							  
+			if (map_base == 0) {  
+					printf("NULL pointer!\n");  
+			}   else  {  
+					printf("Successfull!map_base=%llx\n", map_base);  
+			}  
+											   
+			for (i = 0;i < SIZE; i+=0x10)  {  
+					printf("0x%lx: %8x %8x %8x %8x\n", PHY_BASE+i, *(volatile int*)(map_base+i), 
+						*(volatile int*)(map_base+i+4), *(volatile int*)(map_base+i+8), *(volatile int*)(map_base+i+0xc));  
+																						  
+			}  
+													  
+			close(fd);  
+														  
+			munmap(map_base, SIZE);  
+															  
+			return (1);  
+}  
