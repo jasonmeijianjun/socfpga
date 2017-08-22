65f8db145fafe6db8e31462ca1581801d2a2c57c
Jason Mei <mjjwff@hotmail.com>
2017-08-22 20:53:14 +0800

    Added the preboot source code and it's depended xml file.

---
+++ cv_soc_devkit_ghrd/design_config.tcl
@@ -0,0 +1,16 @@
+set QSYS_NAME soc_system
+set QUARTUS_NAME soc_system
+set SYS_TOP_NAME ghrd_top
+set DEVICE_FAMILY "CYCLONEV"
+set FPGA_DEVICE 5CSXFC6D6F31C6 
+
+#                                               #
+#################################################
+
+#################################################
+#     GHRD features enable                      #
+
+# existance of PCIe
+set PCIE_ENABLE 0
+
+
