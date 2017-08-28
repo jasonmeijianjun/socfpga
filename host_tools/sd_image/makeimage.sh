67f9499e220f910741c9a43b78f4a824d3bafa71
jasonmeijianjun <mjjwff@hotmail.com>
2017-08-28 16:02:38 +0800

    Updated image handle scripts.

--- host_tools/sd_image/makeimage.sh
+++ host_tools/sd_image/makeimage.sh
@@ -1,41 +1,82 @@
 #!/bin/bash
-#generate sd image
-sudo dd if=/dev/zero of=sdimage.img bs=1  count=0 seek=5G
-sudo dd if=mbr.img   of=sdimage.img bs=1M count=3 conv=notrunc 
-#generate vfat image
-sudo dd if=/dev/zero of=vfat.img bs=1 count=0 seek=100M
-#format to vfat
-sudo mkfs.vfat vfat.img
+FAT_SPACE="102400"
+
+# uBoot ENV offset
+SDIMG_UBOOT_ENV_OFFSET="512"
+ENV_BASE_NAME="${UBOOT_CONFIG}"
+
+# Boot partition begin at sector 1024
+# This is required as for c5/a5 mainline uboot hard codes the location
+# of the uboot image in the sdcard to 0xa00 sector
+IMAGE_ROOTFS_ALIGNMENT="1024"
+
+ROOTFS_SIZE="2048000"
+# ROOTFS_SIZE_MOD="524288"
+ROOTFS_SIZE_MOD="16384" #16KiB
+
+# Align partitions
+BOOT_SPACE_ALIGNED=$(expr ${BOOT_SPACE} + ${IMAGE_ROOTFS_ALIGNMENT} - 1)   #(2048 + 1024 -1 = 3071)
+BOOT_SPACE_ALIGNED=$(expr ${BOOT_SPACE_ALIGNED} - ${BOOT_SPACE_ALIGNED} % ${IMAGE_ROOTFS_ALIGNMENT}) #3071 - 3071%1024 = 2048
+FAT_SPACE_ALIGNED=$(expr ${FAT_SPACE} + ${IMAGE_ROOTFS_ALIGNMENT} - 1)   # 102400  + 1024 -1 = 102400 + 1023
+FAT_SPACE_ALIGNED=$(expr ${FAT_SPACE_ALIGNED} - ${FAT_SPACE_ALIGNED} % ${IMAGE_ROOTFS_ALIGNMENT}) # 102400 + 1023 - (102400 + 1023)%1024 = 102400
+
+ROOTFS_SIZE_ALIGNED=$(expr ${ROOTFS_SIZE} \+ ${ROOTFS_SIZE_MOD})  #
+ROOTFS_SIZE_ALIGNED=$(expr ${ROOTFS_SIZE_ALIGNED} \- ${ROOTFS_SIZE_ALIGNED} \% ${ROOTFS_SIZE_MOD})
+
+SDIMG_SIZE=$(expr ${IMAGE_ROOTFS_ALIGNMENT} \+ ${BOOT_SPACE_ALIGNED} \+ ${FAT_SPACE_ALIGNED} \+ ${ROOTFS_SIZE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT})
+
+SDIMG="atca-g400.img"
+rm -f ./${SDIMG}
+dd if=/dev/zero of=${SDIMG} bs=1K count=0 seek=${SDIMG_SIZE} 
+
+# Create partition table
+parted -s ${SDIMG} mklabel msdos
+
+parted -s ${SDIMG} unit KiB mkpart primary fat32 $(expr  ${IMAGE_ROOTFS_ALIGNMENT} \+ ${BOOT_SPACE_ALIGNED}) $(expr ${IMAGE_ROOTFS_ALIGNMENT} \+ ${BOOT_SPACE_ALIGNED} \+ ${FAT_SPACE_ALIGNED})
+
+# set fat partition as bootable for distroboot
+parted -s ${SDIMG} set 1 boot on
+
+# P2: Linux FS partition
+parted -s ${SDIMG} unit KiB mkpart primary $(expr ${IMAGE_ROOTFS_ALIGNMENT} \+ ${BOOT_SPACE_ALIGNED} \+ ${FAT_SPACE_ALIGNED}) $(expr ${IMAGE_ROOTFS_ALIGNMENT} \+ ${BOOT_SPACE_ALIGNED} \+ ${FAT_SPACE_ALIGNED} \+ ${ROOTFS_SIZE_ALIGNED})
+
+# P3: A2 partition for bootloader
+parted -s ${SDIMG} unit KiB mkpart primary ${IMAGE_ROOTFS_ALIGNMENT} $(expr ${BOOT_SPACE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT})
+
+# set part 3 to type a2 for spl / uboot image
+# 446 to partition table, 16 bytes per entry, 4 byte offset to partition type
+echo -ne "\xa2" | dd of=${SDIMG} bs=1 count=1 seek=$(expr 446 + 16 + 16 + 4) conv=notrunc && sync && sync
+
+# Create a vfat image with boot files
+FAT_BLOCKS=$(LC_ALL=C parted -s ${SDIMG} unit b print | awk '/ 1 / { print substr($4, 1, length($4 -1)) / 512 /2 }')
+echo "FAT_BLOCKS" ${FAT_BLOCKS}
+rm -f ./vfat.img
+mkfs.vfat -n "${BOOTDD_VOLUME_ID}" -S 512 -C ./vfat.img ${FAT_BLOCKS}
+
 #copy zImage dtb rtf to vfat image.
-# chameleon96.rbf
-# extlinux
-# image-version-info
-# socfpga_cyclone5_chameleon96.dtb
-# socfpga.dtb
-# soc_system.rbf
-# u-boot.scr
-# zImage
-sudo mcopy -i vfat.img  -s ./kernel/zImage ::/zImage
-sudo mcopy -i vfat.img  -s ./kernel/chameleon96.rbf ::/chameleon96.rbf
-sudo mcopy -i vfat.img  -s ./kernel/extlinux ::/
-sudo mcopy -i vfat.img  -s ./kernel/image-version-info ::/image-version-info
-sudo mcopy -i vfat.img  -s ./kernel/socfpga_cyclone5_chameleon96.dtb ::/socfpga_cyclone5_chameleon96.dtb
-sudo mcopy -i vfat.img  -s ./kernel/u-boot.scr ::/u-boot.scr
+mcopy -i vfat.img  -s ./kernel/zImage ::/zImage
+mcopy -i vfat.img  -s ./kernel/chameleon96.rbf ::/chameleon96.rbf
+mcopy -i vfat.img  -s ./kernel/extlinux ::/
+mcopy -i vfat.img  -s ./kernel/image-version-info ::/image-version-info
+mcopy -i vfat.img  -s ./kernel/socfpga_cyclone5_chameleon96.dtb ::/socfpga_cyclone5_chameleon96.dtb
+mcopy -i vfat.img  -s ./kernel/u-boot.scr ::/u-boot.scr
 
 #Generate root filesystem.
-sudo dd if=/dev/zero of=rootfs.img bs=1 count=0 seek=4G
-
+dd if=/dev/zero of=rootfs.img bs=1K count=0 seek=${ROOTFS_SIZE}
 
 #format to ext3
-sudo mkfs.ext3 rootfs.img
+mkfs.ext3 rootfs.img
 
 #mount to /mnt
-#sudo losetup -d /dev/loop1
-sudo losetup /dev/loop1 rootfs.img
-sudo mount /dev/loop1 /mnt
-sudo cp -a rootfs/* /mnt
-sudo umount /mnt
-sudo losetup -d /dev/loop1
+#  losetup -d /dev/loop1
+losetup /dev/loop1 rootfs.img
+mount /dev/loop1 /mnt
+cp -a rootfs/* /mnt
+umount /mnt
+losetup -d /dev/loop1
 
-sudo dd if=vfat.img    of=sdimage.img bs=1M count=100  seek=3    conv=notrunc
-sudo dd if=rootfs.img  of=sdimage.img bs=1M count=4096 seek=103  conv=notrunc
+dd if=vfat.img of=${SDIMG} bs=1M count=100 seek=3 conv=notrunc
+dd if=rootfs.img of=${SDIMG} bs=1M count=1900 seek=103 conv=notrunc
+dd if=bootloader/preloader-mkpimage.bin of=${SDIMG} bs=256K  seek=4 conv=notrunc
+dd if=bootloader/u-boot.img  of=${SDIMG} bs=256K  seek=5 conv=notrunc
+rm vfat.img rootfs.img
--- make_sd_image.sh
+++ make_sd_image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #1.export toolchain
 export ARCH=arm
-export CROSS_COMPILE=./host_tools/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-#
+export CROSS_COMPILE=`pwd`/host_tools/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-
 
 #2.make proloader
 cd ./cv_soc_devkit_ghrd/software/spl_bsp
 make clean && make
 cd -
 rm -f ./host_tools/sd_image/preloader-mkpimage.bin
-cp ./cv_soc_devkit_ghrd/software/spl_bsp/preloader-mkpimage.bin ./host_tools/sd_image/
+cp ./cv_soc_devkit_ghrd/software/spl_bsp/preloader-mkpimage.bin ./host_tools/sd_image/bootloader
 
 #3. make u-boot
 cd u-boot.git
@@ -17,7 +17,7 @@ make socfpga_chameleon96_defconfig
 make
 cd -
 rm -f ./host_tools/sd_image/u-boot-socfpga_cyclone5.img
-cp ./u-boot.git/u-boot.bin  ./host_tools/sd_image/u-boot-socfpga_cyclone5.img
+cp ./u-boot.git/u-boot.img  ./host_tools/sd_image/bootloader
 
 #4. make kernel
 cd linux-socfpga.git
@@ -26,12 +26,12 @@ make atca-g400_defconfig
 make zImage -j4
 make dtbs
 cd -
-#zImage,u-boot.scr,soc_system.rbf,socfpga.dtb
-rm ./host_tools/sd_image/zImage
-rm ./host_tools/sd_image/socfpga.dtb
+
+#cp zImage,u-boot.scr,soc_system.rbf,socfpga.dtb
+rm -f ./host_tools/sd_image/zImage
+rm -f ./host_tools/sd_image/socfpga.dtb
 cp linux-socfpga.git/arch/arm/boot/zImage ./host_tools/sd_image/kernel/
 cp linux-socfpga.git/arch/arm/boot/dts/socfpga_cyclone5_pcie.dtb ./host_tools/sd_image/kernel/
-cd -
 cd ./host_tools/sd_image
 
 source makeimage.sh
